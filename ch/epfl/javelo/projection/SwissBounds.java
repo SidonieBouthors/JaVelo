5475e5dd3ad7b9cc80c0b75de44e56bc0d9d7092
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-02-22 11:06:45 +0100

    Changes by francois

--- ch/epfl/javelo/projection/SwissBounds.java
+++ ch/epfl/javelo/projection/SwissBounds.java
@@ -1,4 +1,33 @@
 package ch.epfl.javelo.projection;
 
-public class SwissBounds {
+/**
+ * @author Sidonie Bouthors (343678)
+ * @author François Théron (346077)
+ */
+public final class SwissBounds {
+    /**
+     * Non instantiable
+     */
+    private SwissBounds(){}
+
+    public final static double MIN_E = 2485000; //2 485 000
+    public final static double MAX_E = 2834000;
+    public final static double MIN_N = 1075000;
+    public final static double MAX_N = 1296000;
+    public final static double WIDTH= MAX_E-MIN_E;
+    public final static double HEIGHT = MAX_N-MIN_N;
+
+    /**
+     * Returns true iff the point at given coordinates are in
+     * bounds of Switzerland
+     * @param e     : east coordinate
+     * @param n     : north coordinate
+     * @return if the point is in Swiss bounds
+     */
+    public static boolean containsEN(double e, double n) {
+        if (e <= MAX_E && e >= MIN_E && n <= MAX_N && n >= MIN_N) {
+            return true;
+        } else {return false;}
+    }
+
 }
