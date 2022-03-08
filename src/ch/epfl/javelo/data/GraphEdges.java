9fb65534fb04e8c1b90cccf398575dbf4ca580d3
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-03-08 11:13:48 +0100

    Step2Finished

--- src/ch/epfl/javelo/data/GraphEdges.java
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -12,36 +12,67 @@ public record GraphEdges (ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuf
      * @param edgeId    : ID of the edge
      * @return whether the edges direction is inverted
      */
-    boolean isInverted(int edgeId){
+    public boolean isInverted(int edgeId){
         return true;
     }
 
     /**
      * Returns the ID of the destination node of the edge of given ID
-     * @param edgeId
-     * @return
+     * @param edgeId    : ID of the edge
+     * @return ID of the destination node of the edge
      */
-    int targetNodeId(int edgeId){
+    public int targetNodeId(int edgeId){
 
         return edgeId;
     }
-    double length(int edgeId){
+
+    /**
+     * Returns length (in meters) of the edge of given ID
+     * @param edgeId    : ID of the edge
+     * @return length of the edge
+     */
+    public double length(int edgeId){
 
         return 0;
     }
-    double elevationGain(int edgeId){
+
+    /**
+     * Returns the positive elevation gain (in meters) of the edge of given ID
+     * @param edgeId    : ID of the edge
+     * @return positive elevation gain of the edge
+     */
+    public double elevationGain(int edgeId){
 
         return 0;
     }
-    boolean hasProfile(int edgeId){
+
+    /**
+     * Returns true iff the edge of given ID has a profile
+     * @param edgeId    : ID of the edge
+     * @return whether the edge has a profile
+     */
+    public boolean hasProfile(int edgeId){
 
         return false;
     }
-    float[] profileSamples(int edgeId){
+
+    /**
+     * Returns the table of samples of the profile of the edge of given ID
+     * Returns empty table if the edge has no profile
+     * @param edgeId    : ID of the edge
+     * @return table of samples of edge profile
+     */
+    public float[] profileSamples(int edgeId){
 
         return new float[0];
     }
-    int attributesIndex(int edgeId){
+
+    /**
+     * Returns the attributes ID associated to the edge of given ID
+     * @param edgeId    : ID of the edge
+     * @return attributes ID of the edge
+     */
+    public int attributesIndex(int edgeId){
 
         return edgeId;
     }
