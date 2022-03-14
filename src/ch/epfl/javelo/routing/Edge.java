d7f4f9c752ef35df60489809d3144df4a0d47e36
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-03-14 09:22:22 +0100

    Etape4

--- src/ch/epfl/javelo/routing/Edge.java
+++ src/ch/epfl/javelo/routing/Edge.java
@@ -1,5 +1,26 @@
 package ch.epfl.javelo.routing;
 
-public record Edge() {
+import ch.epfl.javelo.data.Graph;
+import ch.epfl.javelo.projection.PointCh;
 
+import java.util.function.DoubleUnaryOperator;
+
+public record Edge(int fromNodeId, int toNodeId, PointCh fromPoint, PointCh toPoint, double length, DoubleUnaryOperator profile) {
+    public static Edge of(Graph graph, int edgeId, int fromNodeId, int toNodeId){
+        //TO DO
+        return null;
+    }
+
+    public double positionClosestTo(PointCh point){
+
+        return 0;
+    }
+    public PointCh pointAt(double position){
+
+        return null;
+    }
+    public double elevationAt(double position){
+
+        return position;
+    }
 }
--- src/ch/epfl/javelo/routing/RoutePoint.java
+++ src/ch/epfl/javelo/routing/RoutePoint.java
@@ -1,4 +1,22 @@
 package ch.epfl.javelo.routing;
 
-public record RoutePoint() {
+import ch.epfl.javelo.projection.PointCh;
+
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
+
+public record RoutePoint(PointCh point, double position, double distanceToReference) {
+    public static final RoutePoint NONE = new RoutePoint(null, NaN, POSITIVE_INFINITY);
+    RoutePoint withPositionShiftedBy(double positionDifference){
+
+        return null;
+    }
+    RoutePoint min(RoutePoint that){
+
+        return that;
+    }
+    RoutePoint min(PointCh thatPoint, double thatPosition, double thatDistanceToReference){
+
+        return null;
+    }
 }
