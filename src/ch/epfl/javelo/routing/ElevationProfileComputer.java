bfcde60eba55d45e8efe8c15f282cf9f0531c2b9
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-03-18 21:51:33 +0100

    Etape4

--- src/ch/epfl/javelo/routing/ElevationProfileComputer.java
+++ src/ch/epfl/javelo/routing/ElevationProfileComputer.java
@@ -19,60 +19,80 @@ public final class ElevationProfileComputer {
      * @param route             : the route
      * @param maxStepLength     : maximum spacing between samples
      * @return the profile of the route
+     * @throws IllegalArgumentException if the maximum spacing is <= 0
      */
     public static ElevationProfile elevationProfile(Route route, double maxStepLength) {
-        Preconditions.checkArgument(maxStepLength>0);
+
+        Preconditions.checkArgument(maxStepLength > 0);
 
         double routeLength = route.length();
-        int nbOfSamples = (int)(Math.ceil(routeLength/maxStepLength)+1);
-        double stepLength = routeLength/(nbOfSamples-1);
+        int nbOfSamples = (int) (Math.ceil(routeLength / maxStepLength) + 1);
+        double stepLength = routeLength / (nbOfSamples - 1);
+        // Creating Original Samples
+        float[] samples = new float[nbOfSamples];
+        double position = stepLength;
+        for (float sample:samples){
+            sample = (float) route.elevationAt(position);
+            position += stepLength;
+        }
 
-        // Creating First Tab
-        float [] samples = new float [nbOfSamples];
+        //Filling in NaN values at start of array
+        int i = 0;
+        while (i < nbOfSamples && Float.isNaN(samples[i])) { i++; }
+        if (i == nbOfSamples) {
+            Arrays.fill(samples, 0);
+            return new ElevationProfile(routeLength, samples);
+        }
+        Arrays.fill(samples, 0, i, samples[i]);
 
+        //Filling in NaN values at end of array
+        int j = nbOfSamples - 1;
+        while (0 <= j && Float.isNaN(samples[j])) { j--; }
+        Arrays.fill(samples, j, nbOfSamples, samples[j]);
 
-
-
-
-            //Proposition pour les deux première étapes
-
-            //Remplir les NaN tout à gauche
-            int i = 0;
-            while (!Float.isNaN(i)) {
-                i++;
-            }
-            if (i == nbOfSamples - 1) {
-                return new ElevationProfile(routeLength, new float[nbOfSamples]);
-            }
-            //trouver l'index de la première valeur valide
-            if (i==nbOfSamples){Arrays.fill(samples, 0);} // si aucun trouvé, remplir de 0
-            Arrays.fill(samples, 0, i, samples[i]); //remplir toutes les valeurs avant celle-ci
-
-            //Remplir les NaN tout à droite
-            int j = nbOfSamples;
-            while (!Float.isNaN(j)) {
-                j--;
-            } //trouver l'index de la dernière valeur valide
-            Arrays.fill(samples, j, nbOfSamples, samples[j]); //remplir toutes les valeurs apres celle-ci
-
-            int firstNaNIndex=-1; int nextCorrectValueIndex; boolean findNext = false;
-            for (int k = 0; k < nbOfSamples; k++) {
-                if (Float.isNaN(samples[k])){ //si on trouve une valeur NaN on garde son index et on se met en mode findNext (trouver la prochaine valeur valide)
-                    firstNaNIndex = k;
-                    findNext = true;
-                } else if (findNext){ //si on trouve la prochaine valeur valide
-                    nextCorrectValueIndex = k; //on stocke son index
-                    double step = 1 / (double)(nextCorrectValueIndex - firstNaNIndex); //on calcule 1 divisé par le nombre de NaN du "trou" qu'on est en train de boucher
-                    double x = step;
-                    for (int l = firstNaNIndex; l < nextCorrectValueIndex; l++) {
-                        //pour chaque NaN du trou, on interpole avec la valeur valide juste avant et juste après
-                        samples[l]=(float)Math2.interpolate(samples[firstNaNIndex-1], samples[nextCorrectValueIndex], x);
-                        x += step;
-                    }
+        int firstNaNIndex = -1;
+        int nextCorrectValueIndex;
+        boolean findNext = false;
+        for (int k = 0; k < nbOfSamples; k++) {
+            if (Float.isNaN(samples[k])) {
+                firstNaNIndex = k;
+                findNext = true;
+            } else if (findNext) {
+                nextCorrectValueIndex = k;
+                double step = 1 / (double) (nextCorrectValueIndex - firstNaNIndex);
+                double x = step;
+                for (int l = firstNaNIndex; l < nextCorrectValueIndex; l++) {
+                    samples[l] = (float) Math2.interpolate(samples[firstNaNIndex - 1], samples[nextCorrectValueIndex], x);
+                    x += step;
                 }
             }
+        }
         return new ElevationProfile(routeLength, samples);
 
+        /*
+        int firstSampleNotNaNFromRight = -1;
+        int firstSampleNotNaNFromLeft = firstSampleNotNaNFromRight;
+        for (int i = 0; i < nbOfSamples; i++) {
+            if (firstSampleNotNaNFromRight == -1 && !Float.isNaN(samples[i])) {
+                firstSampleNotNaNFromRight = i;
+            }
+            if (firstSampleNotNaNFromRight != -1 && Float.isNaN(samples[i])) {
+                samples[i] = (float) Math2.interpolate(route.elevationAt(firstSampleNotNaNFromRight * stepLength), route.elevationAt(routeLength), i * stepLength);
+            }
         }
+        for (int i = 0; i < nbOfSamples; i++) {
+
+            if (firstSampleNotNaNFromLeft == -1 && !Float.isNaN(samples[nbOfSamples - i - 1])) {
+                firstSampleNotNaNFromLeft = nbOfSamples - i - 1;
+            }
+            if (firstSampleNotNaNFromLeft != -1 && Float.isNaN(samples[nbOfSamples - i - 1])) {
+                samples[i] = (float) Math2.interpolate(route.elevationAt(0), route.elevationAt(firstSampleNotNaNFromLeft * stepLength), i * stepLength);
+            }
+        }
+         */
+
+
+    }
 }
+
 
