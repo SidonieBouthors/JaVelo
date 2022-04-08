32c10d3c07dd477e9058aac4483b57fedd2e43d7
ZiZouet <91062845+zizouet@users.noreply.github.com>
2022-04-08 17:10:51 +0200

    Filemanager

---
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -0,0 +1,103 @@
+package ch.epfl.javelo.gui;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javafx.scene.image.Image;
+/**
+ * TileManager
+ *
+ * @author Sidonie Bouthors (343678)
+ * @author François Théron (346077)
+ */
+
+public final  class TileManager {
+    private final static int cacheMemoireSize = 100;
+
+    LinkedHashMap<String, Image> cacheMemoire =
+            new LinkedHashMap<String, Image>() {
+                protected boolean removeEldestEntry(Map.Entry<String, Image> eldest)
+                {
+                    return size() > cacheMemoireSize;
+                }
+            };
+
+    private Path cacheDisque;
+    private String nameOfServer;
+
+    public TileManager(Path cacheDisque, String nameOfServer) {
+        this.nameOfServer=nameOfServer;
+        this.cacheDisque=cacheDisque;
+
+    }
+
+    /**
+     * takes as argument the identity of a tile (of type TileId) and returns its image
+     * @param id
+     * @return
+     */
+    public Image imageForTileAt(TileId id) throws IOException {
+        String zoomPath = cacheDisque.toString()+"/"+id.zoom;
+        String xPath = zoomPath+"/"+id.x;
+        String imageName = xPath+"/"+id.y+".png";
+        File zoomFile = new File(zoomPath);
+        File xPathFile = new File(xPath);
+        File imageNameFile = new File(imageName);
+
+
+        if (cacheMemoire.containsKey(imageName)) {
+            return cacheMemoire.get(imageName);
+        } else if (imageNameFile.exists()) {
+            Image image = new Image(imageName);
+            cacheMemoire.put(imageName,image);
+            return image;
+        } else{
+            //creating directories/file that doesn't exist in cache disque
+            if (!zoomFile.exists()) {
+                zoomFile.mkdirs();
+                xPathFile.mkdirs();
+                imageNameFile.createNewFile();
+            }else if (!xPathFile.exists()) {
+                xPathFile.mkdirs();
+                imageNameFile.createNewFile();
+            } else if (!imageNameFile.exists()) {
+                imageNameFile.createNewFile();
+            }
+
+            String urlString = "https://"+nameOfServer+"/"+id.zoom+"/"+id.x+"/"+id.y+".png";
+            URL u = new URL(urlString);
+            URLConnection c = u.openConnection();
+            c.setRequestProperty("User-Agent", "JaVelo");
+            try (InputStream i = c.getInputStream()) {
+                FileOutputStream a = new FileOutputStream(imageNameFile);
+                i.transferTo(a);
+                a.close();
+            }
+            cacheMemoire.put(imageName,new Image(imageName));
+            return cacheMemoire.get(imageName);
+
+        }
+
+
+    }
+
+    record TileId(int zoom,int x, int y){
+        /**
+         * Returns true if and only if they constitute a valid tile identity
+         * @param zoom
+         * @param x
+         * @param y
+         * @return
+         */
+        public  static boolean isValid(int zoom, int x, int y) {
+            return false;
+        }
+    }
+
+}
