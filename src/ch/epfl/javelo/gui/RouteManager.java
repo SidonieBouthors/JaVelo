a0ecb43e7e345564c9815ee393bf74591446f2ed
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-05-03 10:01:02 +0200

    Etape5

--- src/ch/epfl/javelo/gui/RouteManager.java
+++ src/ch/epfl/javelo/gui/RouteManager.java
@@ -1,11 +1,79 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.WebMercator;
+import ch.epfl.javelo.routing.Route;
 import javafx.beans.property.ObjectProperty;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polyline;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public final class RouteManager {
-    RouteManager (RouteBean routeBean, ObjectProperty<MapViewParameters> mapProperty, Consumer<String> errorSignal){
 
+    private final RouteBean routeBean;
+    private final ObjectProperty<MapViewParameters> mapProperty;
+    private final Consumer<String> errorSignal;
+    private final Pane pane;
+
+    RouteManager (RouteBean routeBean, ObjectProperty<MapViewParameters> mapProperty, Consumer<String> errorSignal){
+        this.routeBean = routeBean;
+        this.mapProperty = mapProperty;
+        this.errorSignal = errorSignal;
+        this.pane = new Pane();
+        pane.setPickOnBounds(false);
+
+        MapViewParameters params = mapProperty.get();
+
+        Polyline routeLine = new Polyline();
+        routeLine.getPoints().setAll(points());
+        routeLine.setLayoutX(params.topLeft().getX());
+        routeLine.setLayoutY(params.topLeft().getY());
+        routeLine.setId("route");
+
+        Circle highlightDisc = new Circle(5);
+        highlightDisc.setId("highlight");
+
+        pane.getChildren().add(routeLine);
+        pane.getChildren().add(highlightDisc);
+
+        highlightDisc.setOnMouseClicked( event -> {
+
+        });
+
+        routeBean.highlightedPositionProperty().addListener(l -> {
+            //positionner et/ou rendre (in)visible le disque
+        });
+        routeBean.routeProperty().addListener(l -> {
+            //positionner et/ou rendre (in)visible le disque
+            //reconstruire totalement et/ou rendre (in)visible la polyligne
+        });
+        mapProperty.addListener( l -> {
+            //positionner et/ou rendre (in)visible le disque
+            // SI LE ZOOM CHANGE
+            //reconstruire totalement et/ou rendre (in)visible la polyligne
+            // SI LA CARTE EST GLISSE MAIS LE ZOOM NE CHANGE PAS
+            // repositionner - sans la reconstruire - la polyligne représentant l'itinéraire
+        });
+
+
+    }
+
+    public Pane pane(){
+        return pane;
+    }
+
+    private List<Double> points(){
+        List<PointCh> pointsCh = routeBean.routeProperty().get().points();
+        List<Double> points = new ArrayList<>();
+
+        for (PointCh pointCh:pointsCh){
+            points.add(WebMercator.x(pointCh.lon()));
+            points.add(WebMercator.y(pointCh.lat()));
+        }
+        return points;
     }
 }
